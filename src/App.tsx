79d8c59168597dc253bdf5d74b7e4f06413e3220
Lucas Angeli <jl.angeli@outlook.com>
2023-11-30 10:23:44 -0300

    feat: created pages and added routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
-import { Home } from './Pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
